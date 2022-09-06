4d90ef5dc274c479843470e26eacd660a8f44ed9
Paul Harris <paul.harris@consensys.net>
2022-09-06 11:27:06 +1000

    try to use libc++

--- build-osx-aarch64.sh
+++ build-osx-aarch64.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 export HOST="aarch64-apple-darwin"
 export CMAKE_OPTIONS="-DCMAKE_OSX_ARCHITECTURES=arm64 -DRUN_HAVE_STD_REGEX=0 -DRUN_HAVE_POSIX_REGEX=0"
 export CFLAGS="-arch arm64"
-export CXXFLAGS="-arch arm64"
+export CXXFLAGS="-arch arm64 -v -stdlib=libc++"
 
 source ./build-osx.sh
 
