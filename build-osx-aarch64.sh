f46dc7f2d59808ebec5e4416957483a0785ef94e
Paul Harris <paul.harris@consensys.net>
2022-09-07 11:33:09 +1000

    attempt to remove regex flags again

--- build-osx-aarch64.sh
+++ build-osx-aarch64.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 
 export HOST="aarch64-apple-darwin"
-export CMAKE_OPTIONS="-DCMAKE_OSX_ARCHITECTURES=arm64 -DRUN_HAVE_STD_REGEX=0 -DRUN_HAVE_POSIX_REGEX=0 -DHAVE_CXX_FLAG_WD654=0"
+export CMAKE_OPTIONS="-DCMAKE_OSX_ARCHITECTURES=arm64 -DHAVE_CXX_FLAG_WD654=0"
 export CFLAGS="-arch arm64"
 export CXXFLAGS="-arch arm64 -v -stdlib=libc++"
 
