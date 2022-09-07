240ea85837d01fe760969d254cc71f5dedfaac9e
Paul Harris <paul.harris@consensys.net>
2022-09-07 11:58:01 +1000

    disable benchmarks

--- build-osx-aarch64.sh
+++ build-osx-aarch64.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 
 export HOST="aarch64-apple-darwin"
-export CMAKE_OPTIONS="-DCMAKE_OSX_ARCHITECTURES=arm64 -DHAVE_CXX_FLAG_WD654=0"
+export CMAKE_OPTIONS="-DCMAKE_OSX_ARCHITECTURES=arm64 -DHAVE_CXX_FLAG_WD654=0 -DLEVELDB_BUILD_BENCHMARKS=0"
 export CFLAGS="-arch arm64"
 export CXXFLAGS="-arch arm64 -v -stdlib=libc++"
 
